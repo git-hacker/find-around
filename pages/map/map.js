4cef04d2cd7687c51fe74aaa21b899ea6c76654d
Topjiaxin <1690003928@qq.com>
2018-01-11 21:40:06 +0800

    添加图标

--- pages/map/map.js
+++ pages/map/map.js
@@ -14,7 +14,7 @@ Page({
     var marker = wx.getStorageSync('markers')
     console.log(marker)
     var markers = [];
-    for (var i in marker){
+    for (var i in marker) {
       var cur = marker[i];
       var obj = {
         id: i,
@@ -23,14 +23,99 @@ Page({
         title: cur.title,
         width: "40",
         height: "40",
-        label: { content: cur.title, color:"#1AAD19"},
-        address: "距离："+cur._distance+"米"
+        label: { content: cur.title, color: "#1AAD19" },
+        address: "距离：" + cur._distance + "米"
       };
-      obj.iconPath = "/img/2.png";
+      if (cur.category.indexOf("报刊亭") != -1) {
+        obj.iconPath = "/img/baoting.png";
+      } else if (cur.category.indexOf("面包甜点") != -1) {
+        obj.iconPath = "/img/mianbao.png";
+      } else if (cur.category.indexOf("冷饮店") != -1) {
+        obj.iconPath = "/img/lengyin.png";
+      } else if (cur.category.indexOf("小吃快餐") != -1) {
+        obj.iconPath = "/img/xiaochi.png";
+      } else if (cur.category.indexOf("超市") != -1) {
+        obj.iconPath = "/img/chaoshi.png";
+      } else if (cur.category.indexOf("农贸市场") != -1) {
+        obj.iconPath = "/img/cai.png";
+      } else if (cur.category.indexOf("邮局") != -1) {
+        obj.iconPath = "/img/youju.png";
+      } else if (cur.category.indexOf("中国电信营业厅") != -1) {
+        obj.iconPath = "/img/dianxin.png";
+      } else if (cur.category.indexOf("中国移动营业厅") != -1) {
+        obj.iconPath = "/img/dianxin.png";
+      } else if (cur.category.indexOf("中国联通营业厅") != -1) {
+        obj.iconPath = "/img/liantong.png";
+      } else if (cur.category.indexOf("洗衣店") != -1) {
+        obj.iconPath = "/img/xiyi.png";
+      } else if (cur.category.indexOf("美容美发") != -1) {
+        obj.iconPath = "/img/meirong.png";
+      } else if (cur.category.indexOf("KTV") != -1) {
+        obj.iconPath = "/img/KTV.png";
+      } else if (cur.category.indexOf("酒吧") != -1) {
+        obj.iconPath = "/img/jiuba.png";
+      } else if (cur.category.indexOf("网吧") != -1) {
+        obj.iconPath = "/img/wangba.png";
+      } else if (cur.category.indexOf("加油站") != -1) {
+        obj.iconPath = "/img/jiayouzhan.png";
+      } else if (cur.category.indexOf("洗车场") != -1) {
+        obj.iconPath = "/img/xiche.png";
+      } else if (cur.category.indexOf("博物馆") != -1) {
+        obj.iconPath = "/img/bowuguan.png";
+      } else if (cur.category.indexOf("科技馆") != -1) {
+        obj.iconPath = "/img/keji.png";
+      } else if (cur.category.indexOf("图书馆") != -1) {
+        obj.iconPath = "/img/tushu.png";
+      } else if (cur.category.indexOf("美术馆") != -1) {
+        obj.iconPath = "/img/meishu.png";
+      } else if (cur.category.indexOf("美术馆") != -1) {
+        obj.iconPath = "/img/meishu.png";
+      } else if (cur.category.indexOf("大学") != -1) {
+        obj.iconPath = "/img/daxue.png";
+      } else if (cur.category.indexOf("中学") != -1) {
+        obj.iconPath = "/img/zhongxue.png";
+      } else if (cur.category.indexOf("小学") != -1) {
+        obj.iconPath = "/img/xiaoxue.png";
+      } else if (cur.category.indexOf("中国银行") != -1) {
+        obj.iconPath = "/img/zhongguo.png";
+      } else if (cur.category.indexOf("建设银行") != -1) {
+        obj.iconPath = "/img/jianshe.png";
+      } else if (cur.category.indexOf("农业银行") != -1) {
+        obj.iconPath = "/img/nongye.png";
+      } else if (cur.category.indexOf("上海银行") != -1) {
+        obj.iconPath = "/img/shanghai.png";
+      } else if (cur.category.indexOf("住宅小区") != -1) {
+        obj.iconPath = "/img/xiaoqu.png";
+      } else if (cur.category.indexOf("别墅") != -1) {
+        obj.iconPath = "/img/bieshu.png";
+      } else if (cur.category.indexOf("服务区") != -1) {
+        obj.iconPath = "/img/fuwuqu.png";
+      } else if (cur.category.indexOf("公共厕所") != -1) {
+        obj.iconPath = "/img/gongce.png";
+      } else if (cur.category.indexOf("公用电话") != -1) {
+        obj.iconPath = "/img/gongyongdianhua.png";
+      } else if (cur.category.indexOf("公交车站") != -1) {
+        obj.iconPath = "/img/gongjiaochezhan.png";
+      } else if (cur.category.indexOf("地铁站") != -1) {
+        obj.iconPath = "/img/ditiezhan.png";
+      } else if (cur.category.indexOf("火车站") != -1) {
+        obj.iconPath = "/img/huochezhan.png";
+      } else if (cur.category.indexOf("长途汽车站") != -1) {
+        obj.iconPath = "/img/changtu.png";
+      } else if (cur.category.indexOf("药房药店") != -1) {
+        obj.iconPath = "/img/yaofang.png";
+      } else if (cur.category.indexOf("综合医院") != -1) {
+        obj.iconPath = "/img/yiyuan.png";
+      } else if (cur.category.indexOf("长途汽车站") != -1) {
+        obj.iconPath = "/img/changtu.png";
+      } else if (cur.category.indexOf("酒店宾馆") != -1) {
+        obj.iconPath = "/img/jiudian.png";
+      }
+
       markers.push(obj);
     }
     var curloc = wx.getStorageSync('curloc')
-    
+
     this.setData({
       latitude: curloc.latitude,
       longitude: curloc.longitude,
@@ -42,44 +127,44 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
-  markertap:function(e){
+  markertap: function (e) {
     var idx = e.markerId;
     var obj = this.data.markers[idx];
     console.log(obj);
@@ -95,9 +180,9 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
-  navto:function(){
+  navto: function () {
     wx.navigateTo({
       url: '/pages/addnew/addnew',
     })
